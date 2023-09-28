fd852fd7cc8f256eaea5134d298e9f74d864e0c5
JYoussouf <joseph@preteckt.com>
2023-09-28 06:33:19 -0400

    add styling sheets and methods

---
+++ assets/js/joseppy.js
@@ -0,0 +1,29 @@
+// Fade-In Animation
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+	if (entry.isIntersecting) {
+	  entry.target.classList.add('fade-in');
+	  observer.unobserve(entry.target); // Stop observing once faded in
+	}
+  });
+}, {
+  threshold: 0.2 // Adjust this threshold value as needed
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+
+// select active
+let navbar = document.querySelector("#home");
+navbar.addEventListener("click", (e) => {
+  if(!e.target.classList.contains("nav-link")) {
+    return;
+  }
+  
+  navbar.querySelectorAll(".nav-link.selected").forEach((item) => item.classList.remove("selected"));
+  e.target.classList.add("selected");
+});
